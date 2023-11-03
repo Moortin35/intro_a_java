17e3516731f9468b979d87e662ffb2e57bc5ee90
Moortin35 <moortindelgado@gmail.com>
2023-11-03 20:24:58 -0300

    agregado test de bucle for

---
+++ test_bucle_for/src/test_bucle_for/Test_bucle_for.java
@@ -0,0 +1,9 @@
+package test_bucle_for;
+
+public class Test_bucle_for {
+    public static void main(String[] args) {
+         for (int i = 1; i <= 10; i++) {
+            System.out.println("Estoy en la vuelta N°: " + i);
+        }
+    }
+}
