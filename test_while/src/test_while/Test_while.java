e8e7a84225cae550758e7f6d2eed3540c879b81a
Moortin35 <moortindelgado@gmail.com>
2023-11-03 19:54:26 -0300

    agregado test de while

---
+++ test_while/src/test_while/Test_while.java
@@ -0,0 +1,16 @@
+
+package test_while;
+
+
+public class Test_while {
+
+
+    public static void main(String[] args) {
+         int contador = 0;
+         while(contador <= 10){
+             System.out.println("Estoy en la vuelta N°: " + contador);
+             contador++;
+         }
+    }
+    
+}
