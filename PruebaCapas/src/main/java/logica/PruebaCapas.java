3b2f73531bb9a212b5c896df82f51e82f63fd84d
Moortin35 <moortindelgado@gmail.com>
2023-11-13 21:13:35 -0300

    agregado prueba de capas

---
+++ PruebaCapas/src/main/java/logica/PruebaCapas.java
@@ -0,0 +1,13 @@
+package logica;
+
+import igu.Pantalla;
+
+public class PruebaCapas {
+
+    public static void main(String[] args) {
+        Pantalla screen = new Pantalla();
+        screen.setVisible(true);
+        screen.setLocationRelativeTo(null);
+        
+    }
+}
