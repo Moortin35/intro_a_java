601bf4f037c6e219b7b1f2b94f92aecc3280ca4b
Moortin35 <moortindelgado@gmail.com>
2023-11-28 08:30:09 -0300

    la agenda electronica funciona correctamente con todas sus funciones

--- EjercicioAgendaElectronica/src/ejercicioagendaelectronica/EjercicioAgendaElectronica.java
+++ EjercicioAgendaElectronica/src/ejercicioagendaelectronica/EjercicioAgendaElectronica.java
@@ -1,9 +1,15 @@
 package ejercicioagendaelectronica;
 
+import igu.Interfaz;
+
 public class EjercicioAgendaElectronica {
 
     public static void main(String[] args) {
 
+        Interfaz interfazGrafica = new Interfaz();
+        interfazGrafica.setVisible(true);
+        interfazGrafica.setLocationRelativeTo(null);
+        
     }
     
 }
